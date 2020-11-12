07ddc94f20a12ae9c2140422a21a39b8b1d3aeff
Henri Sundquist <Henri.Sundquist@rokki.net>
2020-11-12 14:33:26 +0200

    Added new file reader which reads binary data and returns it as a String

---
+++ Compress/src/main/java/compress/BinaryInputReader.java
@@ -0,0 +1,30 @@
+
+package compress;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BinaryInputReader {
+    
+    private String filePath;
+    
+    public BinaryInputReader(String file) {
+        this.filePath = file;
+    }
+    
+    public String readLines() {
+        try {
+            byte[] byteArr = Files.readAllBytes(Paths.get(filePath));
+            
+            String str = "";
+            for (byte byt : byteArr) {
+                str += Integer.toBinaryString((byt & 0xFF) + 0x100).substring(1);
+            }
+            return str;
+        } catch (Exception e) {
+            System.out.println("Exception in BinaryInputReader " + e);
+        }
+        return "";
+    }
+    
+}
