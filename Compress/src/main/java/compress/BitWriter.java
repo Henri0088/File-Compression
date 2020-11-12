c7d1e42bce3570c2c701b4d048037d1a62d0cbcb
Henri Sundquist <Henri.Sundquist@rokki.net>
2020-11-12 14:34:13 +0200

    Fixed a bug where bytes where being written to a file in reverse

--- Compress/src/main/java/compress/BitWriter.java
+++ Compress/src/main/java/compress/BitWriter.java
@@ -3,6 +3,7 @@ package compress;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Arrays;
 import java.util.BitSet;
 
 /**
@@ -22,16 +23,25 @@ public class BitWriter {
      * @param str Binary string to write
      */
     public void writeBits(String str) {
+        if (str.length() % 8 != 0) {
+            int filler = 8 - (str.length() % 8);
+            for (int i = 0; i < filler; i++) {
+                str += "0";
+            }
+        }
+        
         BitSet bits = new BitSet(str.length());
-
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == "1".charAt(0)) {
+            if (str.charAt(i) == '1') {
                 bits.set(i);
             }
         }
-
         byte[] byteArr = bits.toByteArray();
         
+        // toByteArray flips bytes around? 
+        for (int i = 0; i < byteArr.length; i++) {
+            byteArr[i] = reverse(byteArr[i]);
+        }
         try {
             FileOutputStream fos = new FileOutputStream(file);
             fos.write(byteArr);
@@ -39,6 +49,21 @@ public class BitWriter {
         } catch (Exception e) { // Better exception type?
             System.out.println("Writing failed! " + e);
         }
+    }
+    
+    private byte reverse(byte x) {
+        byte newX = 0;
+        for (int i = 0; i < 8; i++) {
+            // Push bits left
+            newX <<= 1;
+            // Get next bit from x
+            byte temp = (byte) (x & 1);
+            // Place bit into newX
+            newX |= temp;
+            // Push bits right
+            x >>= 1;
+        }
+        return newX;
     }
     
 }
