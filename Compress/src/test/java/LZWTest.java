ff0597bb1111d37965c12145ef9d466d361937c9
Henri Sundquist <Henri.Sundquist@rokki.net>
2020-12-17 23:27:23 +0200

    Added randTest

--- Compress/src/test/java/LZWTest.java
+++ Compress/src/test/java/LZWTest.java
@@ -1,6 +1,7 @@
 
 import io.InputReader;
 import compress.LZW;
+import java.util.Random;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,6 +35,22 @@ public class LZWTest {
         String cStr = lzw.compress(str);
         String dStr = lzw.decompress(cStr);
         assertTrue(str.equals(dStr));
+    }
+    
+    @Test
+    public void randTest() {
+        StringBuilder str;
+        Random rnd = new Random();
+        for (int r = 0; r < 5; r++) {
+            str = new StringBuilder();
+            lzw = new LZW();
+            for (int i = 0; i < 10_000; i++) {
+                str.append((char) rnd.nextInt(256));
+            }
+            String original = str.toString();
+            String uncompressed = lzw.decompress(lzw.compress(original));
+            assertTrue(original.equals(uncompressed));
+        }
     }
     
 }
